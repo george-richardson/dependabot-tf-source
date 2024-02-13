a09efd93ad529aec64f3cf740627dbd7b27926ab
George Richardson <george.richardson@scalefactory.com>
2024-02-13 17:11:16 +0000

    initial

---
+++ module-a/main.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_version = ">= 1.4.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 4.57"
+    }
+  }
+}
+
+resource "aws_s3_bucket" "module_a" {
+  bucket_prefix = "module-a"
+}
